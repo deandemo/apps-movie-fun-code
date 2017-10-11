51a055ebf169f68c5e3332b2af2078cbebd30044
Dean Baker <dean.baker@pexa.com.au>
2017-10-11 16:21:48 -0600

    fixing jar to war reference

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -7,14 +7,14 @@ pushd movie-service
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find movie-service/target -type f -name *.jar | wc -l`
+jar_count=`find movie-service/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
-  echo "More than one jar found, don't know which one to deploy. Exiting"
+  echo "More than one war found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find movie-service/target -type f -name *.jar -exec cp "{}" package-output/movie-service.jar \;
+find movie-service/target -type f -name *.war -exec cp "{}" package-output/movie-service.war \;
 
 echo "Done packaging"
 exit 0
