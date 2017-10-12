c64d92f4072c11b5db66dca938f3322446f8ed0a
Dean Baker <dean.baker@pexa.com.au>
2017-10-12 08:52:15 -0600

    Breaking the build

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -4,7 +4,7 @@ set -e +x
 
 pushd movie-service
   echo "Executing tests"
-  MOVIE_FUN_URL=http://movie-service-dutiable-multijet.apps.cumuluslabs.io/ ./mvnw clean test
+  MOVIE_FUN_URL=http://movie-service-dutiable-multijedt.apps.cumuluslabs.io/ ./mvnw clean test
 popd
 
 find movie-service/target/surefire-reports -type f -name *.txt -exec cp "{}" test-output/smoke-test.txt \;
